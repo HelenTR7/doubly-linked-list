d93911208747cb68498f08f4a5448496ead13620
Helen Trapeznikova <31996958+HelenTR7@users.noreply.github.com>
2017-09-27 23:58:00 +0300

    Update linked-list.js

--- src/linked-list.js
+++ src/linked-list.js
@@ -26,24 +26,7 @@ class LinkedList {
 
     tail() {}
 
-     at(index) {//searchNodeAt(position)ищет узел на n-ой позиции в списке
-    var currentNode = this.head,
-        length = this.length,
-        count = 1,
-        message = {failure: 'Failure: non-existent node in this list.'};
-
-    // 1-ый случай: неверная позиция 
-      if (length === 0 || position < 1 || position > length) {
-        throw new Error(message.failure);
-      }
-
-    // 2-ой случай: верная позиция 
-      while (count < position) {
-        currentNode = currentNode.next;
-        count++;
-      }
-
-      return currentNode;
+     at(index) {
     }
 
     insertAt(index, data) {}
