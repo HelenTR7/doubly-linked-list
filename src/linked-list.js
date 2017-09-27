c957128abf3ed5ce4a43bd7fb949ef74a518db6f
Helen Trapeznikova <31996958+HelenTR7@users.noreply.github.com>
2017-09-27 23:48:21 +0300

    Update linked-list.js

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,10 +1,27 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor(length=0, head=null,tail=null) {
+        this.length=length;//извлекает кол-во узлов в списке
+        this.head=head;//назначает узел в качестве головного эл-та списка
+        this.tail=tail;//назначает узел в качестве конечного элемента списка
+    }
 
-    append(data) {}
 
+    append(data) {//add(value)добавляет узел в список
+      var node = new Node(data);
+
+      if(this.length){
+        this.tail.next=node;
+        node.prev=this.tail;
+        this.tail=node;
+      }else{
+        this.head=node;
+        this.tail=node;
+      }
+      this.length++;
+      return node;
+    }
     head() {}
 
     tail() {}
